29bc0dc60882832b877b2c6a2e1e91051d1227fd
Mark <moshea93@gmail.com>
2017-08-07 15:27:19 -0400

    added ww2, and changed from PUMS 2015 to PUMS 2011-2015

--- explore.py
+++ explore.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-vets = pd.read_csv('iraqvets.csv', dtype={'PUMA': object})
-vets = vets.fillna(value=0)
-totalcount = vets.groupby('PUMA').agg({'MLPA': 'count'})
-vetcount = vets[vets['MLPA'] == 1].groupby('PUMA').agg({'MLPA': 'count'})
-average = vets.groupby('PUMA').agg({'MLPA': 'mean'})
-average['MLPA'] = average['MLPA'].divide(average['MLPA'].mean())
-print average.describe()
+def warstatistics(csvfile, csvvariable):
+
+    vets = pd.read_csv(csvfile, dtype={'PUMA10': object})
+    vets = vets.fillna(value=0)
+    totalcount = vets.groupby('PUMA10').agg({csvvariable: 'count'})
+    vetcount = vets[vets[csvvariable] == 1].groupby('PUMA10').agg({csvvariable: 'count'})
+    average = vets.groupby('PUMA10').agg({csvvariable: 'mean'})
+    average[csvvariable] = average[csvvariable].divide(average[csvvariable].mean())
+    print average.describe()
+
+warstatistics('5yearnamvets.csv', 'MLPE')
--- prep.py
+++ prep.py
@@ -6,7 +6,7 @@ import os
 def prep_data(csvfilelist, csvvariables):
     datalist = []
     for csvfile in csvfilelist:
-        chunks = pd.read_csv(csvfile, chunksize=1000000, dtype={'PUMA': object})
+        chunks = pd.read_csv(csvfile, chunksize=1000000, usecols=csvvariables, dtype={'PUMA10': object})
         data = pd.concat(chunk[csvvariables] for chunk in chunks)
         print 'finished narrowing ' + csvfile
         datalist.append(data)
@@ -15,12 +15,12 @@ def prep_data(csvfilelist, csvvariables):
     return output
 
 def get_education_data(csvfile):
-    data = pd.read_csv(csvfile, dtype={'PUMA': object})
+    data = pd.read_csv(csvfile, dtype={'PUMA10': object})
     #N/A is "no active duty", 0 is "did not serve this period" --> 0 is global "did not serve"
     data = data.fillna(value=0)
-    average = data.groupby('PUMA').agg({'MLPA': 'mean'})
-    average['MLPA'] = average['MLPA'].divide(average['MLPA'].mean())
-    value_dict = dict(zip(average.index, average['MLPA']))
+    average = data.groupby('PUMA10').agg({'MLPE': 'mean'})
+    average['MLPE'] = average['MLPE'].divide(average['MLPE'].mean())
+    value_dict = dict(zip(average.index, average['MLPE']))
     return value_dict
 
 def prep_shapefiles(value_dict):
@@ -43,10 +43,12 @@ def prep_shapefiles(value_dict):
             newrecord.append(value_dict[shape.record[1]])
             w.records.append(newrecord)
             w._shapes.append(shape.shape)
-    w.save('us_shapefile/iraqvets/iraq')
+    w.save('us_shapefile/5yearnamvets/nam')
 
 if __name__ == '__main__':
-    output = prep_data(['data/ss15pusa.csv', 'data/ss15pusb.csv'], ['PUMA', 'MLPA'])
-    output.to_csv('iraqvets.csv')
-    value_dict = get_education_data('iraqvets.csv')
+    output = prep_data(['5yeardata/ss15pusa.csv', '5yeardata/ss15pusb.csv', '5yeardata/ss15pusc.csv', '5yeardata/ss15pusd.csv'], ['PUMA10', 'MLPE'])
+    output.to_csv('5yearnamvets.csv')
+    print 'Starting value_dict'
+    value_dict = get_education_data('5yearnamvets.csv')
+    print 'Starting shapefile construction'
     prep_shapefiles(value_dict)
